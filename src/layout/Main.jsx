f5dbf63b7ddfcc7a5a3bff180f55ba2d7494028e
Wasey Rahman <wasey123435@gmail.com>
2023-05-03 16:35:31 +0600

    Banner and home

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navigation/Navigation';
-import Header from '../Header/Header';
+import Banner from '../pages/Home/Banner/Banner';
+import Home from '../pages/Home/Home/Home';
+
+
+
+
 
 const Main = () => {
     return (
         <div>
            <Navigation></Navigation>
-           <Header></Header>
+           <Banner></Banner>
+           <Home></Home>
            <Footer></Footer>
         </div>
     );
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import Apetizer from './Apetizer.jpg';
+import Chicken from './Chiken.jpg';
+import Rice from './Rice.jpg';
+import Vegetable from './Vegetable.jpg';
+import Dal from './Dal.jpg';
+import Kabab from './Kabab.jpg';
+import Dessert from './dessert.jpg';
+
+
+import { Col, Image, Row } from 'react-bootstrap';
 
 const Home = () => {
+    
     return (
-        <div>
-            <h4>Hello</h4>
-        </div>
+        <section>
+            <section>
+            <h2 className='mt-5 text-center text-primary'>Recipe Categories</h2>
+            <Row className='mt-5'>
+                <Col xs={6} md={3}>
+                    <Image src={Apetizer} roundedCircle style={{height:'200px',width:'200px'}} />
+                    <h4 className="mt-2 text-center">Appetizer</h4>
+                </Col>
+                <Col xs={6} md={3}>
+                    <Image src={Chicken} roundedCircle style={{height:'200px',width:'200px'}}/>
+                    <h4 className="mt-2 text-center">Chicken</h4>
+                </Col>
+                <Col xs={6} md={3}>
+                    <Image src={Rice} roundedCircle style={{height:'200px',width:'200px'}}/>
+                    <h4 className="mt-2 text-center">Rice</h4>
+                </Col>
+                <Col xs={6} md={3}>
+                    <Image src={Vegetable} roundedCircle style={{height:'200px',width:'200px'}} />
+                    <h4 className="mt-2 text-center">Vegetable</h4>
+                </Col>
+                <Col xs={6} md={3}>
+                    <Image src={Dal} roundedCircle style={{height:'200px',width:'200px'}} />
+                    <h4 className="mt-2 text-center">Dal</h4>
+                </Col>
+                <Col xs={6} md={3}>
+                    <Image src={Kabab} roundedCircle style={{height:'200px',width:'200px'}} />
+                    <h4 className="mt-2 text-center">Kabab</h4>
+                </Col>
+                <Col xs={6} md={3}>
+                    <Image src={Dessert} roundedCircle style={{height:'200px',width:'200px'}} />
+                    <h4 className="mt-2 text-center">Dessert</h4>
+                </Col>
+                
+              
+            </Row>
+            </section>
+            
+        </section>
+        
+        
+        
+        
+
     );
 };
-
 export default Home;
